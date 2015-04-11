006b7455a1bb3d121150b55f24d53b166d79cc4a
Denis Baylo <denis.baylo@gmail.com>
2015-04-11 20:56:03 +0600

    add base n home templates, conf static n media settings

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from project import settings
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+urlpatterns = patterns('',
+    url(r'^$', 'project.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
-]
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+              static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
